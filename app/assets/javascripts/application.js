9c95c3c2050265d8f8f040104e4732c181d82e61
Adam Williams <kainolophobia@gmail.com>
2012-06-04 22:24:21 -0400

    jquery ui button test contd

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,7 +7,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
-//= require jquery.ui.ipad.altfix
+//= require jquery.ui.touch-punch.min
 //= require underscore
 //= require backbone
 //= require backbone_rails_sync
