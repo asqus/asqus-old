34e8da52d141af3dd5a3d962067f8f76d7b5fb63
Jake <Jake@JakeSchwartz.com>
2012-07-09 15:24:54 -0400

    Dynamic email signup on index page.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,7 +8,6 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require jqm-config
-//=# require jquery.mobile-1.0.1.min
 //= require underscore
 //= require backbone
 //= require backbone_rails_sync
@@ -19,10 +18,41 @@
 //= require_tree .
 
 
-//Prevent scrolling
 $(function() {
+  
+  //Prevent scrolling
   document.body.addEventListener('touchmove', function(event) {
     event.preventDefault();
   }, false);
+
+  emailSignupListener();
+  
 });
+
+
+function emailSignupListener() {
+  $('form.email_signup').on('submit', function(event) {
+    console.log(event.target);
+    var container = $(event.target.parentElement)
+    container.children('form').hide()
+    container.children('.loader').show()
+    $.ajax({
+      type: 'POST',
+      url: '/users/email_signup',
+      data: container.children('form').serialize(),
+      success: function() {
+        container.children('.loader').hide()
+        container.children('.success').show()
+      },
+      error: function() {
+        container.children('.loader').hide()
+        container.children('form').show()
+      }
+    });
+    return false
+  });
+}
+
+
+
 
